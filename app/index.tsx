6306ad85bb8c685dcd5a9c5237aa0045ee377688
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-06-11 12:15:15 +0800

    Add workout page PoC skeleton

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,24 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-export default function Index() {
+export const Index = () => {
+  const insets = useSafeAreaInsets();
+
   return (
     <View
       style={{
+        paddingTop: insets.top,
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
       <Text>Welcome!</Text>
+      <Link href="workouts/push-up">Push up</Link>
+      <Link href="workouts/plank">Plank</Link>
     </View>
   );
 }
+
+export default Index;
---
+++ app/workouts/[workout].tsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import { useLocalSearchParams, useNavigation } from "expo-router";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+const CURRENT_SET = 1;
+const SETS = 2;
+const REPS = 10;
+
+export const Workout = () => {
+  const [set,] = React.useState<number>(CURRENT_SET);
+  const [reps,] = React.useState<number>(REPS);
+
+  const { workout } = useLocalSearchParams();
+  const workoutName = String(workout ?? '');
+
+  const navigation = useNavigation();
+  React.useEffect(() => {
+    navigation.setOptions({ headerTitle: workoutName.toUpperCase() });
+  }, [navigation]);
+
+  return (
+    <View style={styles.container}>
+      <VideoTutorial />
+      <WorkoutProgress title={workoutName} totalSets={SETS} currentSet={set} />
+      <ProgressControl reps={reps} />
+    </View>
+  );
+};
+
+const VideoTutorial = () => (
+  <View style={styles.video}>
+    <Text>(Video section)</Text>
+  </View>
+);
+
+const WorkoutProgress = ({ title, currentSet, totalSets }: { title: string; currentSet: number, totalSets: number }) => (
+  <View style={styles.workoutContainer}>
+    <Text style={styles.workoutTitle}>{title}</Text>
+    <Text>SET {currentSet} / {totalSets}</Text>
+  </View>
+);
+
+const ProgressControl = ({ reps }: { reps: number }) => (
+  <View style={styles.controlContainer}>
+    <Button title="<" onPress={() => {/* move to previous workout */}} />
+    <View style={styles.repsIndicator}>
+      <View>
+        <Text>{reps}</Text>
+      </View>
+      <View>
+        <Text>REPS</Text>
+      </View>
+    </View>
+    <Button title=">" onPress={() => {/* move to next workout */}} />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  video: {
+    width: '100%',
+    height: 240,
+    backgroundColor: '#777',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  workoutContainer: {
+    width: '100%',
+    height: 80,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  workoutTitle: {
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+  },
+  controlContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    gap: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  repsIndicator: {
+    flexDirection: 'column',
+    gap: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 80,
+    height: 80,
+    borderRadius: 80 / 2,
+    backgroundColor: '#FAA',
+  },
+});
+
+export default Workout;
